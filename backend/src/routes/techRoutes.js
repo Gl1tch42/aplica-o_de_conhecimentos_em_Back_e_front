cf46183dc88bc1b9d2a6d1cde1252a8ce7005f0e
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2020-12-02 20:25:59 -0300

    atualizaçao das rotas

--- backend/src/routes/techRoutes.js
+++ backend/src/routes/techRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const techController = require('./../controllers/techController');
 
-router.post('/newCategory',techController.createTech);
-router.get('/get',techController.techList);
-router.put('/update/:id',techController.updateTech);
-router.delete('/delete/:id',techController.deleteTech);
+const techController = require('./../controllers/techController');
+const auth = require('./../middlewares/auth');
+const authAdmin = require('./../middlewares/authAdmin');
+
+router.post('/newCategory',auth ,authAdmin ,techController.createTech);
+router.get('/get', auth,authAdmin ,techController.techList);
+router.put('/update/:id',auth ,authAdmin ,techController.updateTech);
+router.delete('/delete/:id',auth ,authAdmin ,techController.deleteTech);
 
 module.exports = router;
