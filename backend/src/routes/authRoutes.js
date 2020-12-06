95243ed9c7c30c3e756295a2e1c663757c8bef57
Jean Michel Fernandes Carvalho <50302236+Gl1tch42@users.noreply.github.com>
2020-12-06 14:02:09 -0300

    addiçao da rota logout

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('./../middlewares/auth');
 const authtCtrl = require('./../controllers/authController');
 
 router.post('/register',authtCtrl.register);
 router.post('/login', authtCtrl.login);
+router.get('/logout', auth, authtCtrl.logout);
 
 module.exports = router;
